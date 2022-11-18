b1ddf01fba757b2856bbd15689d5a4de638defee
Secretaria <jiomar213@gmail.com>
2022-11-18 16:54:13 -0300

    Inserção de mais funcoes.

--- Ponto Inova/Ponto.py
+++ Ponto Inova/Ponto.py
@@ -54,6 +54,7 @@ def inserir_novo_membro():
     nome = str(input("Digite o nome: ").strip().upper())
     nascimento = str(input("Digite a data de nascimento(dia/mes/ano): "))
     funcao = str(input("Digite  a função: ").strip().upper())
+
     lista_dados = [matricula, nome, nascimento, funcao]
 
     cursor.execute(f"""insert into dados_registro(matricula, nome, nascimento, funcao) values (?,?,?,?)""", lista_dados)
@@ -64,12 +65,12 @@ def inserir_novo_membro():
     conexao.close()
 
 
-def consulta_dados_agenda():
+def consulta_dados_registro():
     import sqlite3
     conexao = sqlite3.connect("dados_registro.db")
     cursor = conexao.cursor()
 
-    cursor.execute("select * from dados_registro")
+    cursor.execute("select * from dados_registro order by matricula")
     resultado = cursor.fetchall()
     for registro in resultado:
         print(f"Matricula:{registro[0]}\nNome: {registro[1]}\nNascimento:{registro[2]}\nFunção:{registro[3]}\n")
@@ -78,5 +79,85 @@ def consulta_dados_agenda():
     conexao.close()
 
 
-inserir_novo_membro()
-consulta_dados_agenda()
+def teste_validacao_matricula():
+    import sqlite3
+    conexao = sqlite3.connect("dados_registro.db")
+    cursor = conexao.cursor()
+
+    lista_matriculas = [] #ARMAZENA TODAS AS MATRICULAS DE dados_registro VALIDAS
+    mat_pesquisa = int(input("digite uma matricula: ")) #VARIAVEL PARA PESQUISA DE UMA MATRIZ
+
+    cursor.execute("select matricula from dados_registro") #SELECIONA TODAS AS MATRICULAS DO BANCO
+    resultado = cursor.fetchall() #ARMAZENA TODAS AS MATRICULAS EM UMA LISTA
+    for registro in resultado: #PERCORRE TODAS AS MATRICULAS EM resultado
+         lista_matriculas.append(registro[0])  #ADICIONA AS MATRICULAS NA LISTA PARA VALIDAR
+
+    if mat_pesquisa in lista_matriculas: #VAZ A VALIDAÇÃO SE A MATRICULA PESQUISADA EXISTE NA LISTA DE MATRICULAS VALIDAS
+        cursor.execute(f"select * from dados_registro where matricula = {mat_pesquisa} order by matricula") #PESQUISA TODOS OS DADOS EM dados_registro DA MATRICULA PESQUISADA
+        resultado = cursor.fetchall() # ARMAZENA OS RESULTADOS DA PESQUISA
+        for dados in resultado:
+            print(f"Matricula: {dados[0]}, Nome: {dados[1]}, Nascimento: {dados[2]}, Função: {dados[3]}\n") #MOSTRA OS DADOS DA MATRICULA PESQUISADA
+    else: #CASO NÃO SEJA UMA MATRICULA VALIDA RETORNA UMA MENSAGEM
+        print("não esta")
+
+    cursor.close()
+    conexao.close()
+
+
+def consulta_registro_ponto():
+    import sqlite3
+    conexao = sqlite3.connect("registro_ponto.db")
+    cursor = conexao.cursor()
+
+    cursor.execute("select * from registro_ponto order by matricula")
+    resultado = cursor.fetchall()
+    for registro in resultado:
+        print(f"Matricula:{registro[0]}\nDia: {registro[1]}\nEntrada1:{registro[2]}\n")
+
+    cursor.close()
+    conexao.close()
+
+
+def consulta_registro_ponto_hoje():
+    import datetime as dt
+    import sqlite3
+
+    agora = dt.datetime.now()
+    data_hoje = agora.strftime("%d/%m/%Y")
+    conexao = sqlite3.connect("registro_ponto.db")
+    cursor = conexao.cursor()
+
+    cursor.execute(f"select * from registro_ponto where dia like '{data_hoje}' order by matricula")
+    resultado = cursor.fetchall()
+    for registro in resultado:
+        print(f"Matricula:{registro[0]};Dia: {registro[1]};Entrada1:{registro[2]}\n")
+
+    cursor.close()
+    conexao.close()
+
+def inserir_entrada_1_em_registro_ponto():
+    import datetime as dt
+    import sqlite3
+
+    conexao = sqlite3.connect("registro_ponto.db")
+    cursor = conexao.cursor()
+
+    matricula = int(input("Digite a matricula: "))
+    agora = dt.datetime.now()
+    data_hoje = agora.strftime("%d/%m/%Y")
+    hora_agora = agora.strftime("%H:%M:%S")
+
+
+    lista_dados = [matricula, data_hoje, hora_agora]
+
+    cursor.execute(f"""insert into registro_ponto(matricula, dia, entrada_1) values (?,?,?)""", lista_dados)
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+
+print("registros hoje")
+consulta_registro_ponto_hoje()
+print("registros gerais")
+consulta_registro_ponto()
