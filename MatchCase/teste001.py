e9f278ce1cf89d3b34b204083019e5bd25e04047
Secretaria <jiomar213@gmail.com>
2023-01-16 12:23:24 -0300

    Midificações basicas.

---
+++ MatchCase/teste001.py
@@ -0,0 +1,10 @@
+opcao = int(input('Digite sua opção: '))
+
+match opcao:
+    case 1: print(1)
+    case 2: print(2)
+    case 3: print(3)
+    case 4: print(4)
+    case 5: print(5)
+    case 6: print(6)
+    case 7: print(7)
